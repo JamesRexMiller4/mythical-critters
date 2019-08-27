d30e8b2738266c3f93c1695f00173328643f934e
James Miller <james.rex.miller.4@gmail.com>
2019-08-26 19:30:02 -0600

    Complete dragon exercise with passing test

---
+++ exercises/dragon.js
@@ -0,0 +1,24 @@
+class Dragon {
+  constructor(name, rider, color) {
+    this.name = name
+    this.rider = rider
+    this.color = color
+    this.hungry = true
+    this.meals = 0
+  }
+  eat() {
+    if (this.meals < 2) {
+      this.meals += 1
+    } else this.hungry = false
+  }
+
+
+
+
+
+
+}
+
+
+
+module.exports = Dragon;
